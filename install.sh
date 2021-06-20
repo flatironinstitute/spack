cc7b902fb3c353f0ac6b83cd6957a5fdef459f9a
Dylan Simon <dylan@dylex.net>
2021-06-20 16:47:18 -0400

    install: add some options and slurm support
    though parallel building still doesn't work quite right

--- install.sh
+++ install.sh
@@ -1,17 +1,45 @@
 #!/bin/bash -e
+#SBATCH -c 8
+
+while getopts 'fgj:' o ; do case $o in
+	(f) full=1 ;;
+	(g) gc=1 ;;
+	(j) njobs="$OPTARG" ;;
+	(*)
+		echo "Usage: ./install.sh [-g] [-f] [-j N]"
+		echo "       sbatch -n N install.sh [-g] [-f]"
+		echo "Build spack modules. Can be used as an sbatch script."
+		echo " -g    gc first"
+		echo " -f    concretize -f"
+		echo " -j N  parallel jobs"
+		exit 1
+esac ; done
 
 export LC_ALL=en_US.UTF-8 # work around spack bugs processing log files
 source share/spack/setup-env.sh
 
+if [[ $gc ]] ; then
+	spack gc -y || true
+fi
+
+if [[ -n $SLURM_JOB_ID ]] ; then
+	spack_install() {
+		srun -K0 -W0 -k spack -l install -j ${njobs:-${SLURM_CPUS_PER_TASK:-40}} "$@"
+	}
+else
+	spack_install() {
+		spack install ${njobs:+-j $njobs} "$@"
+	}
+fi
+
 echo '*** Bootstrapping compilers'
 spack env activate -V bootstrap
-spack install || { spack env view regenerate && spack install; }
+spack_install || { spack env view regenerate && spack_install --fail-fast; }
 
 echo '*** Building modules'
 spack env activate -V modules
-spack concretize -f
-#spack gc -y || true
-spack install --only-concrete --fail-fast
+spack concretize ${full:+-f}
+spack_install --only-concrete --fail-fast
 
 spack_ls () {
 	spack find -cx --format '{name}@{version}/{hash:7}' "$@" | sort
