5def520af4c327059ae749460f370fca08e902bc
Dylan Simon <dylan@dylex.net>
2025-03-18 13:23:04 -0400

    py-pyarrow: fix last merge

--- var/spack/repos/builtin/packages/py-pyarrow/package.py
+++ var/spack/repos/builtin/packages/py-pyarrow/package.py
@@ -106,11 +106,7 @@ class PyPyarrow(PythonPackage):
         env.set("PYARROW_WITH_ORC", self.spec.satisfies("^arrow+orc"))
         env.set("PYARROW_WITH_DATASET", self.spec.satisfies("^arrow+dataset"))
 
-<<<<<<< HEAD
     @when("@:16")
-=======
-    @when("@:15")
->>>>>>> origin/fi-pkgs
     def install_options(self, spec, prefix):
         args = []
         if spec.satisfies("^arrow+parquet"):
